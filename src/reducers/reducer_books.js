d35701c2fbb9618da65d2dc92a8f1d7a29b9b1f0
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-06-23 12:24:23 -0700

    added test books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    "use strict";
+    return [
+        {title: 'JavaScript: The Ugly Parts'},
+        {title: 'Unshaven Clayshaper'},
+        {title: 'The Light Pawn'},
+        {title: 'Element....Elephant...Elm Treatment...Nicely Written Ruby'}
+    ]
+};
