ad7595f31f0e64dd3cea2a5722298b890c137eb0
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-07-05 11:52:41 -0700

    started adding selectBook action creator
    added selectBook action creator skeleton
    mapped selectBook to BookList.props
    (not currently attached to any events)

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -2,19 +2,13 @@
 import React, {Component} from 'react';
 import BookListItem from '../components/book_list_item';
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
-    bookList(){
-        const list = [];
-        for (let i = 0; i < 4; i++){
-            list.push({title: `Book #${i + 1}`});
-        }
-        return list;
-    }
-
     renderList(){
         return this.props.books.map(function(book){
-            return (<BookListItem key={Math.random()} book={book}/>);
+            return (<BookListItem key={Math.random()} book={book} />);
         });
     }
 
@@ -35,5 +29,11 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch){
+    // Whenever selectBook is called, the result should be passed to all reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
 
+// Promote BookList from a component to a container. It needs to know about this new dispatch method, selectBook. Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
